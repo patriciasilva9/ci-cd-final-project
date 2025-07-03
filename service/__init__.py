d3f5d593711273cff85f1c7f0b86cd6b1b397a8d
patriciasilva9 <patricianobresilva9@gmail.com>
2025-07-03 06:21:27 -0400

    making sure linting task will work part3

--- service/__init__.py
+++ service/__init__.py
@@ -6,11 +6,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # This must be imported after the Flask app is created
-from service import routes               # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers  # pylint: disable=wrong-import-position
+# pylint: disable=wrong-import-position,cyclic-import
+from service import routes            
+# pylint: disable=wrong-import-position   
+from service.common import log_handlers  
 
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info("SERVICE RUNNING".center(70, "*"))
 app.logger.info(70 * "*")
--- service/routes.py
+++ service/routes.py
@@ -99,7 +99,7 @@ def update_counters(name):
 
     if name not in COUNTER:
         return abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Counter {name} does not exist")
 
     COUNTER[name] += 1
