f6ad7d559b2a1cef4d184a5fc10ffb91098b906a
patriciasilva9 <patricianobresilva9@gmail.com>
2025-07-03 06:16:43 -0400

    making sure linting task will work part2

--- service/routes.py
+++ service/routes.py
@@ -40,8 +40,9 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], \
-    counter=count[1]) for count in COUNTER.items()]
+    counters = [dict(
+        name=count[0],
+        counter=count[1]) for count in COUNTER.items()]
 
     return jsonify(counters)
 
@@ -55,13 +56,15 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, \
-        f"Counter {name} already exists")
+        return abort(
+            status.HTTP_409_CONFLICT,
+            f"Counter {name} already exists")
 
     COUNTER[name] = 0
 
-    location_url = url_for("read_counters", \
-    name=name, _external=True)
+    location_url = url_for(
+        "read_counters",
+        name=name, _external=True)
     return (
         jsonify(name=name, counter=0),
         status.HTTP_201_CREATED,
@@ -78,8 +81,9 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, \
-        f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist")
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -94,8 +98,9 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, \
-        f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND, 
+            f"Counter {name} does not exist")
 
     COUNTER[name] += 1
 
